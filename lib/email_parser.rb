42cc0a4a4ed1a0cff7ad248b85551aa6ccaedc10
Claire Harmer <claire.f.harmer@gmail.com>
2018-08-15 02:14:31 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,5 +10,27 @@ def initialize(email)
 end
 
   def parse(email)
+    p_array = @email.split(",")
+
+
+
   end
+end
+
+
+
+
+
+
+
+def parse
+  email_array = emails.split(/[,\s]/)
+  unique_emails = []
+  email_array.each do |e|
+    if !unique_emails.include?(e) && e != ""
+      unique_emails << e
+    end
+  end
+  unique_emails
+end
 end
